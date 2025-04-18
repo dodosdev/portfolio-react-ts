e773ef1b6acf73e03efe50a6b2d548f27c2fab76
dojin <djsong33@gmail.com>
2025-04-18 11:01:11 +0900

    main text 수정

--- src/components/content/Home2.tsx
+++ src/components/content/Home2.tsx
@@ -10,9 +10,9 @@ const Home2: React.FC<HomeProps> = ({logo}) => {
         <div>
             <section id="home">
             <img className="home__avatar" src={logo.img} alt="portfolio" />
-            <h2 className="home__title">Hello <br/> I'm 
-                <strong className="home__title--strong"> Frontend Developer</strong>, {logo.name}</h2>
-            <p className="home__description">A software engineer currently residing in Seoul, South Korea</p>
+            <h2 className="home__title">
+                <strong className="home__title--strong"> Frontend Developer</strong> {logo.name}</h2>
+            <p className="home__description">I am a Frontend developer with a passion for creating beautiful and responsive websites.</p>
             <a className = "home__contact" href="#contact">Contact Me</a>
         </section>
         </div>
