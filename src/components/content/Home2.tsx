9b4625ec6f20c1b705ee6677e1a7310d75c1d9c8
dojin <djsong33@gmail.com>
2025-04-21 14:14:43 +0900

    main copy 수정

--- src/components/content/Home2.tsx
+++ src/components/content/Home2.tsx
@@ -9,12 +9,12 @@ const Home2: React.FC<HomeProps> = ({logo}) => {
     return (
         <div>
             <section id="home">
-            <img className="home__avatar" src={logo.img} alt="portfolio" />
-            <h2 className="home__title">
-                <strong className="home__title--strong"> Frontend Developer</strong> {logo.name}</h2>
-            <p className="home__description">I am a Frontend developer with a passion for creating beautiful and responsive websites.</p>
-            <a className = "home__contact" href="#contact">Contact Me</a>
-        </section>
+                <img className="home__avatar" src={logo.img} alt="portfolio" />
+                <h2 className="home__title">
+                    <strong className="home__title--strong"> Frontend Developer</strong> {logo.name}</h2>
+                <p className="home__description">I'm a Frontend developer with a passion for creating beautiful and responsive websites.</p>
+                <a className = "home__contact" href="#contact">Contact Me</a>
+            </section>
         </div>
     );
 }
